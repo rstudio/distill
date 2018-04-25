5178ca9780ddf34d16fe894ae6286c4a453adca2
jjallaire <jj@rstudio.com>
2018-04-25 06:34:02 -0400

    add support for footnotes

--- inst/rmarkdown/templates/distill_article/resources/distill-1.0/distill.js
+++ inst/rmarkdown/templates/distill_article/resources/distill-1.0/distill.js
@@ -2,12 +2,26 @@
 
 window.document.addEventListener("DOMContentLoaded", function (event) {
 
+  // replace citations with <dt-cite>
   $('.citation>a').each(function(i, val) {
     var href = $(this).attr('href');
     var key = href.replace('#ref-', '');
     var cite = $('<dt-cite></dt-cite>');
     cite.attr('key', key);
     $(this).parent().replaceWith(cite);
-  })
+  });
+
+  // replace footnotes with <dt-fn>
+  $('.footnote-ref').each(function(i, val) {
+    var href = $(this).attr('href');
+    var id = href.replace('#', '');
+    var fn = $('#' + id);
+    var fn_p = $('#fn1>p');
+    fn_p.find('.footnote-back').remove();
+    var text = fn_p.text();
+    var dtfn = $('<dt-fn></dt-fn>');
+    dtfn.text(text);
+    $(this).replaceWith(dtfn);
+  });
 
 });
