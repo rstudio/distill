8afd93d471d6c5281e63389a4b32e5844cad2b6b
jjallaire <jj@rstudio.com>
2018-05-23 14:59:38 -0400

    refactor navigation functions to produce html objects

--- R/navigation.R
+++ R/navigation.R
@@ -1,24 +1,38 @@
 
 
 navigation_in_header <- function(site_config, metadata) {
-
-  # if we have a site navbar
-  if (!is.null(site_config[["navbar"]])) {
-
-    # dependency on headroom.js for auto-hide navbar
-    knitr::knit_meta_add(list(html_dependency_headroom()))
-
-    # css and javascript
-    system.file("rmarkdown/templates/radix_article/resources/navbar.html",
-                 package = "radix")
-
-  } else {
-    navigation_placeholder("in_header")
-  }
+  render_navigation_html(navigation_in_header_html(site_config, metadata))
 }
 
-
 navigation_before_body <- function(site_config, metadata) {
+  render_navigation_html(navigation_before_body_html(site_config, metadata))
+}
+
+navigation_after_body <- function(input_file, site_config, metadata) {
+  render_navigation_html(navigation_after_body_html(input_file, site_config, metadata))
+}
+
+navigation_in_header_html <- function(site_config, metadata) {
+
+  if (!is.null(site_config[["navbar"]])) {
+
+    in_header_html <- html_from_file(
+      system.file("rmarkdown/templates/radix_article/resources/navbar.html",
+                  package = "radix")
+    )
+
+    in_header_html <- attachDependencies(in_header_html, list(html_dependency_headroom()))
+
+
+  } else {
+    in_header_html <- navigation_placeholder_html("in_header")
+  }
+
+  in_header_html
+
+}
+
+navigation_before_body_html <- function(site_config, metadata) {
 
   # helper to yield icon class
   icon_class <- function(icon) {
@@ -34,8 +48,8 @@ navigation_before_body <- function(site_config, metadata) {
     build_menu <- function(menu) {
       item_to_menu <- function(item) {
         if (!is.null(item[["icon"]])) {
-          knitr::knit_meta_add(list(html_dependency_font_awesome()))
           icon <- tag("i", list(class = icon_class(item[["icon"]])))
+          icon <- attachDependencies(icon, html_dependency_font_awesome())
         } else {
           icon <- NULL
         }
@@ -93,19 +107,14 @@ navigation_before_body <- function(site_config, metadata) {
     ))
   }
 
-  # write and return file
-  header_html <- renderTags(tagList(
+  tagList(
     HTML(navigation_begin('before_body')),
     header,
     HTML(navigation_end('before_body'))
-  ), indent = FALSE)$html
-  header_file <- tempfile(fileext = "html")
-  writeLines(header_html, header_file)
-  header_file
+  )
 }
 
-
-navigation_after_body <- function(input_file, site_config, metadata) {
+navigation_after_body_html <- function(input_file, site_config, metadata) {
   site_dir <- input_as_dir(input_file)
   footer <- file.path(site_dir, "footer.html")
   if (!is.null(site_config$navbar) && file.exists(footer)) {
@@ -120,22 +129,26 @@ navigation_after_body <- function(input_file, site_config, metadata) {
       options = list("--template", pandoc_path_arg(footer_template),
                      "--metadata", "pagetitle:footer")
     )
-    footer_html
+    html_from_file(footer_html)
   } else {
-    navigation_placeholder("after_body")
+    navigation_placeholder_html("after_body")
   }
 }
 
 
 navigation_placeholder <- function(context) {
   placeholder <- tempfile(fileext = "html")
-  writeLines(c(
-    navigation_begin(context),
-    navigation_end(context)
-  ), con = placeholder)
+  writeLines(renderTags(navigation_placeholder(context))$html, placeholder)
   placeholder
 }
 
+navigation_placeholder_html <- function(context) {
+  HTML(paste(
+    navigation_begin(context),
+    navigation_end(context),
+    sep = "\n")
+  )
+}
 
 navigation_begin <- function(context) {
   paste0('<!--radix_navigation_', context ,'-->')
@@ -145,5 +158,14 @@ navigation_end <- function(context) {
   paste0('<!--/radix_navigation_', context, '-->')
 }
 
+html_from_file <- function(file) {
+  HTML(paste(readLines(file), collapse = "\n"))
+}
 
-
+render_navigation_html <- function(navigation_html) {
+  rendered <- renderTags(navigation_html, indent = FALSE)
+  knitr::knit_meta_add(rendered$dependencies)
+  file <- tempfile(fileext = "html")
+  writeLines(rendered$html, file)
+  file
+}
