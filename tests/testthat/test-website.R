e02701933d3886343af250228ce93950efcc419b
jjallaire <jj@rstudio.com>
2018-08-26 13:25:43 -0400

    correct test cleanup

--- tests/testthat/test-website.R
+++ tests/testthat/test-website.R
@@ -3,15 +3,15 @@ context("website")
 test_that("websites can be created", {
   skip_on_cran()
   expect_error({
+    on.exit(unlink("testsite", recursive = TRUE), add = TRUE)
     create_website("testsite", "Test Site", edit = FALSE)
-    unlink("testsite")
   }, NA)
 })
 
 test_that("blogs can be created", {
   skip_on_cran()
   expect_error({
+    on.exit(unlink("testblog", recursive = TRUE), add = TRUE)
     create_blog("testblog", "Test Blog", edit = FALSE)
-    unlink("testblog")
   }, NA)
 })
