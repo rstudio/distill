5557f517fd61d602ef63f1cf2b663ef1a2da6082
jjallaire <jj@rstudio.com>
2018-09-09 06:50:08 -0400

    use preview rather than daily build url

--- R/rstudio.R
+++ R/rstudio.R
@@ -18,7 +18,7 @@ validate_rstudio_version <- function() {
       if (rstudio$version < "1.2.718")
         stop("Radix articles cannot be previewed in this version of RStudio.\n",
              "Please update to version 1.2.718 or higher at ",
-             "https://dailies.rstudio.com/\n",
+             "https://www.rstudio.com/products/rstudio/download/preview/\n",
              call. = FALSE)
     }
   }
