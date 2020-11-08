8020e8215bc687b32077a0df48504985c153dbe3
JJ Allaire <jj@rstudio.com>
2020-11-08 10:30:25 -0500

    use lubridate workaround for R 4.0.3 TZDIR issue

--- R/utils.R
+++ R/utils.R
@@ -72,15 +72,8 @@ parse_date <- function(date) {
 }
 
 safe_timezone <- function() {
-  # if TZDIR is set to "internal" or "macOS" on R 4.0.3 then lubridate
-  # will throw an error. This will need to be fixed either in
-  # lubridate or R, in the meantime force to UTC in this config
-  if (getRversion() == "4.0.3") {
-    "UTC"
-  } else {
-    tz <- Sys.timezone()
-    ifelse(is.na(tz), "UTC", tz)
-  }
+  tz <- Sys.timezone()
+  ifelse(is.na(tz), "UTC", tz)
 }
 
 time_as_iso_8601 <- function(time) {
