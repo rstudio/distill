89d0b00c89014134d2b6b096905455f32c7c8c56
christophe dervieux <christophe.dervieux@gmail.com>
2021-02-15 22:31:30 +0100

    Revert "Use CRAN lubridate and remove workaround (#316)"
    This reverts commit 7c567224c89f709d6d34fe82d89a9db4072cc6de.
    It seems the CRAN version of lubridate does not contain the fix it
    says.

--- R/utils.R
+++ R/utils.R
@@ -72,8 +72,15 @@ parse_date <- function(date) {
 }
 
 safe_timezone <- function() {
-  tz <- Sys.timezone()
-  ifelse(is.na(tz), "UTC", tz)
+  # OS X Catalina (10.15.7) has a corrupt timezone database, protect
+  # against this by always returning UTC on OSX until we have a
+  # lubridate fix for this on CRAN
+  if (is_osx()) {
+    "UTC"
+  } else {
+    tz <- Sys.timezone()
+    ifelse(is.na(tz), "UTC", tz)
+  }
 }
 
 time_as_iso_8601 <- function(time) {
